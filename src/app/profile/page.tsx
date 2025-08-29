446d60ba9eb252e3b5fdb80fb5bfd5af4d2eff28
Joey Cadieux <joeycadieux161@gmail.com>
2025-08-29 10:22:34 +0000

    profile page take time to load

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -117,16 +117,32 @@ export default function ProfilePage() {
                         <Skeleton className="h-8 w-3/4" />
                         <Skeleton className="h-4 w-1/2" />
                     </CardHeader>
-                    <CardContent className="space-y-4">
-                        <div className="space-y-2">
-                           <Skeleton className="h-4 w-1/4" />
-                           <Skeleton className="h-10 w-full" />
+                    <CardContent className="space-y-6">
+                        <div className="space-y-4">
+                            <div className="space-y-2">
+                               <Skeleton className="h-4 w-1/4" />
+                               <Skeleton className="h-10 w-full" />
+                            </div>
+                             <div className="space-y-2">
+                               <Skeleton className="h-4 w-1/4" />
+                               <Skeleton className="h-10 w-full" />
+                            </div>
                         </div>
-                         <div className="space-y-2">
-                           <Skeleton className="h-4 w-1/4" />
-                           <Skeleton className="h-10 w-full" />
+                        <Separator />
+                        <div className="space-y-4">
+                             <div className="space-y-2">
+                               <Skeleton className="h-6 w-1/3" />
+                               <Skeleton className="h-4 w-2/3" />
+                            </div>
+                             <div className="space-y-2">
+                               <Skeleton className="h-4 w-1/4" />
+                               <Skeleton className="h-10 w-full" />
+                            </div>
                         </div>
                     </CardContent>
+                    <CardFooter>
+                         <Skeleton className="h-10 w-32" />
+                    </CardFooter>
                  </Card>
             </main>
         </div>
